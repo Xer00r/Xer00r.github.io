db848a432304197e5c5ac87cb87e1cf9073a7a4c
Xer00r <git.cybergod.repo@gmail.com>
2022-04-03 23:13:07 +0100

    some more code

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,6 +1,18 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const team = localStorage.getItem("team")
-    const nameSpan = document.querySelector('span')
-    const { manager } = JSON.parse(team)
-    nameSpan.textContent = manager + '!'
+    const toggleButton = document.getElementById("toggle")
+    const navbar = document.querySelector(".sidebar")
+    const mainContent = document.getElementById("main-content")
+    let isNavOpen = true
+    
+    toggleButton.addEventListener("click", () => {
+        if(!isNavOpen) {
+            navbar.classList.add("show-nav")
+            mainContent.classList.remove("nav-closed")
+            isNavOpen = true
+        } else {
+            navbar.classList.remove("show-nav")
+            mainContent.classList.add("nav-closed")
+            isNavOpen = false
+        }
+    })
 })
