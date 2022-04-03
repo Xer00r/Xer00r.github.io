fb619b88c69c3bfc8843eefc351cc1e9acc5e8b2
Xer00r <git.cybergod.repo@gmail.com>
2022-04-04 00:08:14 +0100

    more bug fixes

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,10 +2,18 @@ window.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.getElementById("toggle")
     const navbar = document.querySelector(".sidebar")
     const mainContent = document.getElementById("main-content")
+    const logoutButton = document.getElementById("sign-out-btn")
     let isNavOpen = true
-    
+
+    logoutButton.addEventListener("click", logout)
+
+    function logout() {
+        localStorage.removeItem("team")
+        window.location.href = `./login.html`
+    }
+
     toggleButton.addEventListener("click", () => {
-        if(!isNavOpen) {
+        if (!isNavOpen) {
             navbar.classList.add("show-nav")
             mainContent.classList.remove("nav-closed")
             isNavOpen = true
