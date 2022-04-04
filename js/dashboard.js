29b78c66bec7d6906fc752775e9e56deae30d14a
Xer00r <git.cybergod.repo@gmail.com>
2022-04-04 02:49:59 +0100

    Added hamburger menu functionality

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const toggleButton = document.getElementById("toggle")
+    const toggleButton = document.querySelector(".hamburger")
+    const burger = document.querySelector(".burger")
     const navbar = document.querySelector(".sidebar")
     const mainContent = document.getElementById("main-content")
     const logoutButton = document.getElementById("sign-out-btn")
@@ -12,14 +13,16 @@ window.addEventListener("DOMContentLoaded", () => {
         window.location.href = `./login.html`
     }
 
-    toggleButton.addEventListener("click", () => {
+    toggleButton.addEventListener("click", (e) => {
         if (!isNavOpen) {
             navbar.classList.add("show-nav")
             mainContent.classList.remove("nav-closed")
+            burger.classList.add("active")
             isNavOpen = true
         } else {
             navbar.classList.remove("show-nav")
             mainContent.classList.add("nav-closed")
+            burger.classList.remove("active")
             isNavOpen = false
         }
     })
