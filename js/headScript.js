3762d7dce3e8413a6d6414b3ab9f6ff16b18d2bf
Xer00r <git.cybergod.repo@gmail.com>
2022-04-05 11:03:41 +0100

    minor bug fix

--- js/headScript.js
+++ js/headScript.js
@@ -1,17 +1,8 @@
-const team = localStorage.getItem("team")
+const url = `https://kobis-global-server.herokuapp.com/api/v1/teams/${id}`
 
-if (team) {
-    const { _id } = JSON.parse(team)
-
-    localStorage.setItem("team", getTeamInfo(_id))
-} else {
-    window.location.href = `./login.html`
-}
-
-async function getTeamInfo(id) {
-    const url = `https://kobis-global-server.herokuapp.com/api/v1/teams/${id}`
-
-    const response = await fetch(url)
-
-    return await response.json()
-}
+fetch(url)
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => {
+    console.log(err)
+})
